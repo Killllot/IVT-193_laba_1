24eebf0baa62098a51cb438d36f2d0969e8bae19
fillont <fillontino@gmail.com>
2020-09-28 10:23:05 +0300

    First version

---
+++ ClassLibrary1/Plus.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ClassLibrary1
+{
+    public class Plus
+    {
+        private string a;
+        private string b;
+
+        public Plus(string a, string b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+
+       
+
+        public string GetSum(string a, string b)
+        {
+            return "" + (double.Parse(a) + double.Parse(b));
+        }
+
+    }
+}
